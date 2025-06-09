2eb61e49599e59cf29cb4e1aede7fc8fcfa228e6
Yaduveer <yaduveerpavan@gmail.com>
2025-06-09 13:39:07 +0530

    Update search.py

--- search.py
+++ search.py
@@ -2,17 +2,20 @@ class search:
     def __init__(self, main_string = None, search_string = None):
         self.main_string = main_string
         self.search_string = search_string
+
     def get_input(self):
         if self.main_string == None:
             self.main_string = input("Enter the main string: ")
         if self.search_string == None:
             self.search_string = input("Enter the string to search for: ")
+
     def perform_search(self):
         position = self.main_string.find(self.search_string)
         if position != -1:
             return(f"'{self.search_string}' found at position {position}")
         else:
             return(f"'{self.search_string}' not found in the main string.")
+        
 if __name__ == "__main__":
     s = search()
     s.get_input()
